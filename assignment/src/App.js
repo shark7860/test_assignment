17d1ff697109a5dc26f07728755c360bce019a88
shark7860 <80542229+shark7860@users.noreply.github.com>
2021-11-18 22:16:25 +0530

    final commit

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line react-hooks/exhaustive-deps
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
@@ -6,7 +5,9 @@ function App() {
   const [searchText, setSearchText] = useState();
   const [token, setToken] = useState();
   const [callapi, setCallapi] = useState(1);
-  const [suggestions, setSuggestions] = useState(1);
+  const [suggestions, setSuggestions] = useState();
+  const [flag, setFlag] = useState(false);
+  const [suggestionList, setSuggestionList] = useState();
   useEffect(() => {
     if (callapi === 1) {
       fetch('https://devcore02.cimet.io/v1/generate-token', {
@@ -25,6 +26,12 @@ function App() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [callapi]);
+  useEffect(()=>{
+if(suggestions){
+  setFlag(true);
+  setSuggestionList(suggestions.data);
+}
+  },[suggestions])
   const fetchSearchSuggestion=(text)=>{
     fetch('https://devcore02.cimet.io/v1/search-address', {
       method: 'POST',
@@ -36,27 +43,50 @@ function App() {
       headers: {
         'Api-key': '4NKQ3-815C2-8T5Q2-16318-55301',
         'Auth-token':`${token}`,
+        'Content-Type': 'application/json',
       },
     })
       .then(response => response.json())
       .then(data => setSuggestions(data));
-    setCallapi(0);
   };
   const changeInput = (e) => {
     const input = e.target.value;
     setSearchText(input);
     fetchSearchSuggestion(input);
   }
-  console.log(suggestions);
+  const handleEnter=(term)=>{
+     setSearchText(term);
+     setFlag(false);
+  };
   return (
-
-    <div className="App">
+    <div>
+      <div className='heading'>Autocomplete Dropdown</div>
+    <div className="autocomplete searchbar">
       <input
         type="text"
         placeholder="Tap here to search"
         value={searchText}
+        className="search-box1 ellipse"
         onChange={changeInput}
       />
+      {flag && suggestionList && suggestionList.length>0&&
+              <div className='search-suggestion' >
+                <ul>
+                  {suggestionList?.map((item) => {
+                    return <li className='search-suggestion-list' style={{left: '11px'}} onClick={()=>handleEnter(item.DisplayLine)}><p>
+                      {item.DisplayLine}
+                    </p></li>;
+                  })}
+                </ul>
+              </div>
+            }
+            <button
+              data-testid="reset-search"
+              className="close-icon lt"
+              type="reset"
+              onClick={() => setSearchText('')}
+            ></button>
+    </div>
     </div>
   );
 }
